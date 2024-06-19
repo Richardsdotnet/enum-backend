9abc22b7a88911ebf0b2ec04ef9418369acd22ab
Akaabiam Richards <richardsakaabiam@gmail.com>
2024-06-19 13:45:13 +0100

    updated

--- src/main/java/com/semicolon/enum_backend/dto/request/CreateUserRequest.java
+++ src/main/java/com/semicolon/enum_backend/dto/request/CreateUserRequest.java
@@ -1,4 +1,28 @@
 package com.semicolon.enum_backend.dto.request;
 
+import com.semicolon.enum_backend.models.Course;
+import com.semicolon.enum_backend.models.Role;
+import com.semicolon.enum_backend.models.Status;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Setter
+@Getter
+
 public class CreateUserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Role role;
+    private Long organizationId;
+    private int noOfPrograms;
+    private List<Course> courses;
+    private Status userStatus;
+    private LocalDate dateAdded;
+    private LocalDate lastActivity;
 }
--- src/main/java/com/semicolon/enum_backend/dto/response/CreateUserResponse.java
+++ src/main/java/com/semicolon/enum_backend/dto/response/CreateUserResponse.java
@@ -1,4 +1,13 @@
 package com.semicolon.enum_backend.dto.response;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
 
 public class CreateUserResponse {
+    private String message;
+
 }
